57393a4b0d75f69d01ca5d878e4b6de8c0ef230e
Tetsuya Mori <monry84@gmail.com>
2017-08-17 02:24:14 +0900

    :new::white_check_mark: Add test case
    * テストケースを追加します。
    * SceneManager.LoadScene を複数回実行しても AudioListener のインスタンスが1つのみであるかどうかを確認します。

---
+++ Assets/Tests/Scripts/SingletonAudioListenerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests {
+
+    public class SingletonAudioListenerTest {
+
+        [UnityTest]
+        public IEnumerator BasicTest() {
+            SceneManager.LoadScene("Tests/Scenes/Test", LoadSceneMode.Single);
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            Assert.AreEqual(1, Object.FindObjectsOfType<AudioListener>().Length);
+            SceneManager.LoadScene("Tests/Scenes/Test", LoadSceneMode.Additive);
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            Assert.AreEqual(1, Object.FindObjectsOfType<AudioListener>().Length);
+        }
+
+    }
+
+}
