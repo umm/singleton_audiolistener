26d1e6088c4ae3971fa70de1aa05e592bd35dd42
Tetsuya Mori <monry84@gmail.com>
2017-08-17 01:52:14 +0900

    :seedling: Initial commit

---
+++ Assets/Scripts/UnityModule/SingletonAudioListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityModule {
+
+    public class SingletonAudioListener : MonoBehaviour {
+
+        private void Start() {
+            if (FindObjectOfType<AudioListener>() == default(AudioListener)) {
+                this.gameObject.AddComponent<AudioListener>();
+            }
+        }
+
+    }
+
+}
